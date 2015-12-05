6f9596b29f8c6b92019c16567adf15c310e023e4
Dmitriy <dbkai@ya.ru>
2015-12-06 00:02:39 +0500

    Add icon font

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 var matrixARows = 3;
-var matrixACells = 3;
+var matrixAColumns = 3;
 var matrixBRows = 3;
-var matrixBCells = 3;
-var matrixCRows = 3;
-var matrixCCells = 3;
+var matrixBColumns = 3;
 
-function insertCell(matrix, x, y) {
+function CreateMatrix(matrixName, rows , columns) {
+  for (var i = 1; i <= rows; i++) {
+    document.write(insertRow(matrixName, i));
+    for (var j = 1; j <= columns; j++) {
+      document.write(insertColumn(matrixName, i, j));
+    }
+    document.write("</div>");
+  }
+}
+
+function ClearMatrix() {
+  var matrix = null;
+  var rows = 0;
+  var columns = 0;
+
+  for (var m = 1; m <= 2; m++) {
+    matrix = m === 1 ? 'A' : 'B';
+    rows = m === 1 ? matrixARows : matrixBRows;
+    columns = m === 1 ? matrixAColumns : matrixBColumns;
+
+    for (var i = 1; i <= rows; i++) {
+      for (var j = 1; j <= columns; j++) {
+
+        var id = "matrix_cell" + matrix + i + j;
+
+        document.getElementById(id).value = null;
+      }
+    }
+  }
+}
+
+function insertColumn(matrix, x, y) {
   var id = "matrix_cell" + matrix + x + y;
-  var change = "rem_cell(1," + x + "," + y + ")";
   var placeholder = matrix.toLowerCase() + x + "." + y;
-  var disabled = matrix === 'C'?"disabled":"";
+  var disabled = matrix === 'C' ? "disabled" : "";
 
-  return "<input type=\"text\" class=\"matrix_cell\" placeholder=\"" + placeholder + "\" size=\"16\" id=\"" + id + "\" value=\"\" onchange=\"" + change + "\" autocomplete=\"off\"" + disabled + ">";
+  return "<input type=\"text\" class=\"matrix_cell\" placeholder=\"" + placeholder + "\" size=\"16\" id=\"" + id + "\" value=\"\" autocomplete=\"off\"" + disabled + ">";
 }
 
 function insertRow(matrix, x) {
@@ -30,5 +58,6 @@ function Multiplication_Click(control) {
 
 function addRow(matrix) {
 
-  
+
 }
+
